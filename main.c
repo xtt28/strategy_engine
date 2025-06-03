f4b3b726c98c70ee30a782e6ea52eb0624ba4833
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-06-02 20:20:52 -0400

    Create user interface and add project meta files.

--- main.c
+++ main.c
@@ -45,10 +45,10 @@ utility_function utility_function_create(payoff_matrix matrix, char wrt) {
   int offset = wrt == 'x' ? 0 : 1;
   utility_function ufunc;
 
-  double zero_zero = matrix.rows[0].values[0].payoffs[offset];
-  double zero_one = matrix.rows[0].values[1].payoffs[offset];
-  double one_zero = matrix.rows[1].values[0].payoffs[offset];
-  double one_one = matrix.rows[1].values[1].payoffs[offset];
+  float zero_zero = matrix.rows[0].values[0].payoffs[offset];
+  float zero_one = matrix.rows[0].values[1].payoffs[offset];
+  float one_zero = matrix.rows[1].values[0].payoffs[offset];
+  float one_one = matrix.rows[1].values[1].payoffs[offset];
 
   ufunc.xy = zero_zero - zero_one - one_zero + one_one;
   ufunc.y = one_zero - one_one;
@@ -58,18 +58,32 @@ utility_function utility_function_create(payoff_matrix matrix, char wrt) {
   return ufunc;
 }
 
+char* utility_function_tostring(utility_function func) {
+  char* string;
+  asprintf(&string, "%.2fxy + %.2fx + %.2fy + %.2f", func.xy, func.x, func.y, func.con);
+  return string;
+}
+
 int main(void) {
-  payoff_entry zero_zero = { { 15, 10 } };
-  payoff_entry zero_one = { { 5, 5 } };
-  payoff_entry one_zero = { { 0, 5 } };
-  payoff_entry one_one = { { 10, 20 } };
-  payoff_matrix_row row_zero = { zero_zero, zero_one };
-  payoff_matrix_row row_one = { one_zero, one_one };
-  payoff_matrix matrix = { { row_zero, row_one } };
-  utility_function func_x = utility_function_create(matrix, 'x');
-  utility_function func_y = utility_function_create(matrix, 'y');
-  printf("util 1: %.2fxy + %.2fx + %.2fy + %.2f\n", func_x.xy, func_x.x, func_x.y, func_x.con);
-  float y = utility_function_findcritical(func_x, 'x');
-  float x = utility_function_findcritical(func_y, 'y');
-  printf("x=%.2f,y=%.2f\n", x, y);
+  puts("Payoff matrix data entry");
+  payoff_matrix matrix;
+
+  for (int i = 0; i < sizeof(matrix.rows) / sizeof(matrix.rows[0]); ++i) {
+    printf("Row %d\n", i + 1);
+    for (int j = 0; j < 2; ++j) {
+      payoff_entry entry;
+      printf("Entry %d (separate with spaces): ", j + 1);
+      scanf("%f %f", &entry.payoffs[0], &entry.payoffs[1]);
+      matrix.rows[i].values[j] = entry;
+    }
+  }
+
+  utility_function util_x = utility_function_create(matrix, 'x');
+  utility_function util_y = utility_function_create(matrix, 'y');  
+  
+  printf("f_1(x, y) = %s\n", utility_function_tostring(util_x));
+  printf("f_2(x, y) = %s\n\n", utility_function_tostring(util_y));
+
+  printf("x = %.2f\n", utility_function_findcritical(util_y, 'y'));
+  printf("y = %.2f\n", utility_function_findcritical(util_x, 'x'));
 }
